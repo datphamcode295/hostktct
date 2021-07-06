ce45c0b974593ddfb940101d6561498454af9c2d
thanhmv <thanhmv@ahamove.com>
2021-07-06 23:25:48 +0700

    fix bug interval

--- src/components/questions/countdown.js
+++ src/components/questions/countdown.js
@@ -1,51 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function CountDown(props){
     
-    // const [count, setCount] = useState(2);
-    
-    // var timeInterval;
-    // const timefunction = () => {
-    //     if(count){
-    //         setCount(count - 1);
-    //         console.log(count);
-    //     }
-    //     else {
-    //         clearInterval(timeInterval);
-    //     }}
-    // const handleClickTime = () => {timeInterval = setInterval(timefunction, 1000);}
-    // if(count==0)
-    // // console.log("a");
-    // clearInterval(timeInterval);
-    // return (<div>
-        
-    //         {count}
-    //         <button onClick={handleClickTime}>Click!!!</button>
-    // </div>)
+  const [count, setCount] = useState(4);
+  const [timerOn, setTimerOn] = useState(false);
 
-let timeout = 0
-const [count, setCount] = useState(2);
-const  handleTiming = ()=>{
-    timeout = setInterval(() => {
-        if(count){
+  let interval = null
 
-            setCount(count - 1);
-            console.log(count);
-        }
-        
-        
-    }, 1000);
-    if(Interval(timeout);
-}
+  useEffect(() => {
+    if(timerOn && count > 0) {
+        interval = setInterval(() => {
+        setCount(count => count - 1);
+      }, 1000);
+    }
+    else {
+      clearInterval(interval)
+    }
+    return () => {
+      clearInterval(interval);
+    }
+  }, [count, timerOn]);
 
-return (
-  <div>
-    <p>{count}</p>
-    <button onClick={handleTiming}>
-      Click 
-    </button>
-  </div>
-);
+  const turnOn = () => {
+    setTimerOn(true)
+  }
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => turnOn()}>
+        Click 
+      </button>
+    </div>
+  );
 }
 
 export default CountDown;
